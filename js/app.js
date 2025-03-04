18c934124d7b2e2c93ff90b9e740a8511b77eadc
nboyce <boycenobukho@gmail.com>
2025-03-04 02:32:19 +0200

    api integration for the forecast + (made too many api request i'm getting 409)

--- js/app.js
+++ js/app.js
@@ -43,15 +43,23 @@ document.addEventListener("DOMContentLoaded", function() {
 
         let timeStamp = responseData.time;
         displayDayTime(timeStamp);
+        getForecast(responseData.city);
 
     }
 
     function searchCity(city){
         // Get the data from the API
-        let apiKey = "b2a5adcct04b33178913oc335f405433";
+        let apiKey = "8827643caa23f3fa4e0oc6dt3bdbc467";
         let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
         axios.get(apiUrl).then(displayTemperature);
+    }
+
+    function getForecast(city){
+        //Get forecast data from the API
+        let apiKey = "8827643caa23f3fa4e0oc6dt3bdbc467";
+        let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+        axios.get(apiUrl).then(displayForecast);
     }
 
     function handleSubmission(event){
@@ -61,8 +69,38 @@ document.addEventListener("DOMContentLoaded", function() {
         searchCity(formInput);
     }
 
+    function displayForecast(response){
+        dataResponse = response.data;
+        console.log(dataResponse);
+
+        let forecastString = "";
+
+        //let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+        //let forecastIconElement = `<img src="${dataResponse.daily.condition.icon_url}" class="weather-icon"/>`;
+
+        response.data.daily.forEach(function(day, index){
+            if (index <= 5){
+                let forecastIconElement = `<img src="${day.condition.icon_url}" class="weather-icon"/>`;
+                
+                forecastString += `<div class="forecast-days">
+                <div class="forecast-day">Tue</div>
+                    <div class="forecast-icon">${forecastIconElement}</div>
+                    <div class="forecast-temperature">
+                        <div class="forecast-max"><strong>${Math.round(day.temperature.maximum)}°C </strong></div> 
+                        <div class="forecast-min"> ${Math.round(day.temperature.minimum)}°C</div>
+                    </div>
+                    </div>`;
+            }
+        });
+        
+        let forecastElement = document.querySelector(".weather-forecast");
+        forecastElement.innerHTML = forecastString;
+    }
+
     let formSearch = document.querySelector("#search-form");
     formSearch.addEventListener("submit", handleSubmission);
     searchCity("Cape Town");
+    displayForecast();
 
 });
